600285f8bdb87a16ef2aaac2ee9df7a58140cd37
ayyaz10 <codementee@outlook.com>
2024-05-01 17:18:34 +0500

    isolated handy button with ShadowRoot

--- content.js
+++ content.js
@@ -1,14 +1,56 @@
 console.log("chrome extension go");
 
+function createCustomElement(element) {
+  return document.createElement(element);
+}
+
 function injectButton() {
-  var button = document.createElement("button");
-  button.textContent = "Injected Button";
-  button.id = "injectedButton";
-  document.body.appendChild(button);
+  let div = document.createElement("div");
+  div.classList.add("handy-shadow-container");
+  document.body.appendChild(div);
+  let host = document.querySelector(".handy-shadow-container");
+  let root = host.attachShadow({ mode: "open" });
+  let handyButton = createCustomElement("div");
+
+  const imageUrl = chrome.runtime.getURL("focuslist-logo.png");
+  handyButton.innerHTML = `
+    <div class="handylist-button-container">
+      <img class="handylist-logo" src="${imageUrl}" width="80" height="80" alt="myimage">
+      <a href="#" class="handylist-button" id="injectedButton">Handy List</a>
+    </div>
+    <style>
+      .handylist-button-container {
+        position: fixed;
+        bottom: 40%;
+        right: 2%;
+        z-index: 999;
+      }
+      
+      .handylist-button {
+        border: none;
+        border-radius: 50px;
+        background: white;
+        color: black;
+        font-size: 15px;
+      
+        /* display: block; */
+      }
+      
+      .handylist-logo {
+        border: 1px solid red;
+        padding: 20px;
+      }
+    </style>
+  `;
+  console.log(root);
+  root.appendChild(handyButton);
+  // document.body.insertAdjacentElement("afterend", handyButton);
+
+  // document.body.appendChild(handyButton);
 }
 
 function removeButton() {
-  var button = document.getElementById("injectedButton");
+  var button = document.querySelector(".handy-shadow-container");
   if (button) {
     button.remove();
   }
@@ -26,7 +68,6 @@ chrome.storage.sync.get("showButtonState", function (data) {
   handleButtonVisibility(data.showButtonState || false);
 });
 chrome.runtime.onMessage.addListener(function (message) {
-  console.log(message);
   if (message.showButton !== undefined) {
     handleButtonVisibility(message.showButton);
   }
