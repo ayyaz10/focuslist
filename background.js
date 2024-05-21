55cb41fb5b02679c41eaca635d70e6e375421602
ayyaz10 <codementee@outlook.com>
2024-05-21 23:36:28 +0500

    update

--- background.js
+++ background.js
@@ -1,50 +1,19 @@
-// console.log("background js go");
-// chrome.storage.sync.get(null, function (data) {
-//   console.log("Stored Data:", data);
-// });
-// chrome.action.onClicked.addListener(buttonClicked);
-
-// function buttonClicked(tab) {
-//   console.log(tab.id);
-//   let msg = { txt: "hello" };
-//   chrome.tabs.sendMessage(tab.id, msg, (response) => {
-//     console.log("message sent");
-//   });
-// }
-
-// chrome.action
-// chrome.message
-
-// chrome.action.onClicked.addListener((request, sender, sendResponse) => {
-//   console.log(request);
-//   sendResponse({ farewell: "goodbye" });
-// });
-
-// chrome.runtime.onInstalled.addListener(() => {
-//   chrome.tabs.create({
-//     url: "https://chat.openai.com",
+// chrome.action.onClicked.addListener((tab) => {
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     files: ["content.js"],
 //   });
 // });
 
-// Set up alarm for task reminder
-// chrome.alarms.create("taskReminder", {
-//   periodInMinutes: 0.05, // Set reminder after 1 hour (can be changed as needed)
-// });
-
-// // Listen for alarm trigger
-// chrome.alarms.onAlarm.addListener((alarm) => {
-//   if (alarm.name === "taskReminder") {
-//     // Display reminder notification
-//     // console.log("Task Reminder");
-//     chrome.notifications.create({
-//       type: "basic",
-//       iconUrl: "focuslist-logo.png",
-//       title: "Task Reminder",
-//       message: "Don't forget to complete your task!",
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   if (message.clearLocalStorage) {
+//     // Clear local storage
+//     chrome.storage.local.clear(function () {
+//       console.log("Local storage cleared");
+//       // Send a response indicating success or failure
+//       sendResponse({ success: true });
 //     });
+//     // Return true to indicate that sendResponse will be called asynchronously
+//     return true;
 //   }
 // });
-
-chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  // console.log(tabs);
-});
